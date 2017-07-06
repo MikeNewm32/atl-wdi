5c6b3a5f0a376f3172199adb1ca8694b790d9392
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-06 17:41:02 -0400

    functions lab starter through question 4

--- labs/unit_01/javaScript/js-functions/functions-lab-starter.js
+++ labs/unit_01/javaScript/js-functions/functions-lab-starter.js
@@ -1,27 +1,49 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(num1,num2) {
+    if (num1 > num2) {
+        return num1;
+    } else if (num1 < num2) {
+        return num2;
+    }
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(num1,num2,num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num1 < num2 && num2 > num3) {
+        return num2;
+    } else if (num1 < num3 && num2 < num3) {
+        return num3;
+    }
 }
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(letter) {
+    var vowels = ['a', 'e', 'i', 'o', 'u']
+    if (vowels.indexOf(letter.toLowerCase()) !== -1) {
+        return true;
+    }
+    return false;
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 }
 
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(numbers) {
+  var multiply = numbers[0];
+  for (var i = 1; i < numbers.length; i++) {
+    multiply *= numbers[i];
+  }
+  return multiply;
 }
 
 
