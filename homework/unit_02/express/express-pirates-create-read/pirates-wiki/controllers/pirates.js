f997ee1f0e753bf805cfebcb26d4720007afb3f6
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-20 23:55:13 -0400

    HW attempt

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,19 @@
 //your code below
+
+//requirements
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+
+//all routes for /pirate
+router.get('/', (req, res) => {
+    data: ('pirates/index')
+});
+router.post('/', (req, res) => {
+    data.seededPirates.push(newPirate);
+    res.redirect('/pirates');
+});
+
+//exports
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,20 @@
 //your code here
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+
+var pirateController = require('./controllers/pirates.js');
+app.use("/pirates", pirateController);
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,  () => {
+    console.log('pirates have landed on port ' + port);
+});
