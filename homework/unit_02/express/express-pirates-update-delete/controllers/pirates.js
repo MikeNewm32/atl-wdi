91b98b9772b4381c538cd3bf0b8e55f060160172
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-24 09:42:02 -0400

    stuck

--- homework/unit_02/express/express-pirates-update-delete/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-update-delete/controllers/pirates.js
@@ -37,15 +37,54 @@ router.get('/:id', function(req, res){
 //==============================
 // CREATE
 //==============================
-
+router.get('/new', (req, res) => {
+  res.render('pirates/new');
+})
 //==============================
 // UPDATE
 //==============================
+router.get('/:id/edit', (req, res) => {
+  const showPirate = req.params.id;
+  const pirates = pirates.pirates[id];
+  res.render("pirates/edit", {
+    name: name,
+    id: id
+  })
+});
+router.put('/:id', (req, res) => {
+  // We have the ID
+  const id = req.params.id;
+  // Use the id to grab specific index in array
+  const pirates = pirates.pirates[id];
+  // Update the property values
+  pirates.description = req.body.description;
+  pirates.urgent = req.body.urgent;
+  // redirect back to individual name
+  res.method = "GET";
+  res.redirect(`/pirates/${id}`);
+});
+router.post('/', (req, res) => {
+  console.log(req.body);
+
+  const pirates = {
+    property: req.body.property
+
+  };
+  pirates.pirates.push(pirates);
+
+  res.redirect(“/pirates”);
+});
 
 //==============================
 // DESTROY
 //==============================
+router.delete('/:id', (req, res) => {
 
+    pirates.pirates.splice(req.params.id, 1);
+
+    res.method= "GET";
+    res.redirect("/pirates");
+});
 //==============================
 // EXPORTS
 //==============================
