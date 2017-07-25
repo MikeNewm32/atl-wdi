c77b865e97f8b0fa0cd815ee770e0df92033da9d
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-24 22:43:16 -0400

    part 1: created tamagotchi constructor

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -2,8 +2,27 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 
+class Tamagotchi {
+    constructor(){
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+}
+    cry() {
+        this.foodInTummy--;
+        console.log("WAHH!!!!")
+        console.log('Tamagotchi omly has ' + this.foodInTummy + ' food in its tummy');
+    }
+}
 
 //create new Tamagotchis
 
+var tamogotchiNum1 = new Tamagotchi();
+var tamogotchiNum2 = new Tamagotchi();
+
 
 //test out your Tamagotchies below via console.logs
+console.log(tamogotchiNum1);
+console.log(tamogotchiNum2);
+tamogotchiNum1.cry();
+tamogotchiNum2.cry();
--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -2,21 +2,44 @@
 
 const fs = require('fs');
 
+console.log(process.argv);
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
+const readFilePromise = new Promise((resolve, reject) => {
+  fs.readFile(inFile, {encoding: 'utf8'}, (err, content) => {
+    if (err) {
+      reject(err);
+    }
+    resolve(content);
+  });
+});
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
-
-  let lines = content.split('\n');
-
-  // clean up the array by removing the empty line
-  lines.pop();
-
+readFilePromise
+.then(data => data.split('\n'))
+.then(lines => {
+  lines.pop()
   lines.forEach(function(line) {
     console.log('Hello, ' + line + '!');
   });
+})
+.catch(err => {
+  console.log("There was an error!");
+  console.log(err);
 });
+
+// fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//   if (error) {
+//     console.error(error);
+//   }
+
+//   // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+
+//   let lines = content.split('\n');
+
+//   // clean up the array by removing the empty line
+//   lines.pop();
+
+//   lines.forEach(function(line) {
+//     console.log('Hello, ' + line + '!');
+//   });
+// });
