f1b8983074887617b6bfd01a4dfb96235c6b8652
MikeNewm32 <michaelnewman85@hotmail.com>
2017-08-18 12:30:08 -0400

    class lab work

--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -2,19 +2,19 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+SELECT * FROM books JOIN authors ON books.author_id = authors.id WHERE authors.name = 'George R.R. Martin';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+SELECT * FROM books JOIN authors ON books.author_id = authors.id WHERE authors.name = 'Milan Kundera';
 -- Find all books written by an author from China or the UK.
-
+SELECT title FROM books JOIN authors ON books.author_id = authors.id WHERE authors.nationality IN ('China', 'United Kingdom');
 -- Find out how many books Albert Camus wrote.
-
+SELECT authors.name, COUNT(books.title) AS books FROM books JOIN authors ON books.author_id = authors.id WHERE authors.name = 'Albert Camus' GROUP BY authors.name;
 -- Find out how many books were written by US authors.
-
+SELECT authors.nationality, COUNT(books.title) AS books FROM books JOIN authors ON books.author_id = authors.id WHERE authors.nationality = 'United States of America' GROUP BY authors.nationality;
 -- Find all books written after 1930 by authors from Argentina.
-
+SELECT title FROM books JOIN authors ON books.author_id = authors.id WHERE authors.nationality = 'Argentina' AND books.publication_date > 1930;
 -- Find all books written before 1980 by authors not from the US.
-
+SELECT title FROM books JOIN authors ON books.author_id = authors.id WHERE authors.nationality != 'United States of America' AND books.publication_date < 1980;
 -- Find all authors whose names start with 'J'.
 
 -- Find all books whose titles contain 'the'.
--- labs/sql/library_sql/basic_queries.sql
+++ labs/sql/library_sql/basic_queries.sql
@@ -5,7 +5,6 @@
 -- Get all information about all authors
 SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-<<<<<<< HEAD
 SELECT name, birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
 SELECT name FROM authors WHERE birth_year >= 1901;
@@ -13,7 +12,6 @@ SELECT name FROM authors WHERE birth_year >= 1901;
 SELECT * FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
 SELECT * FROM books WHERE publication_date = 1985; 
-=======
 
 -- Get all authors born in the 20th century or later
 
@@ -21,18 +19,14 @@ SELECT * FROM books WHERE publication_date = 1985;
 
 -- Get all books published in 1985
 
->>>>>>> f6a9420fe749572f73141822de567a889b5d6e37
 -- Get all books published before 1989
 SELECT * FROM books WHERE publication_date <=1989;
 -- Get just the title of all books.
 SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
-<<<<<<< HEAD
   -- Cry when you realize how long it's been
 SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
-=======
 
->>>>>>> f6a9420fe749572f73141822de567a889b5d6e37
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
 SELECT * FROM books WHERE title ILIKE '% the %' OR title ILIKE '%the %';
 -- Add yourself as an author
