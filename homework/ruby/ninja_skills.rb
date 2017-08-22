9f4b744d3be853b9ef05da6d4c79b026c128f79d
MikeNewm32 <michaelnewman85@hotmail.com>
2017-08-21 23:14:13 -0400

    6. Arrays skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -33,8 +33,18 @@
 # puts "Multiply what number?"
 # multiply = gets.chomp
 # puts multiply.to_i * 2
-puts "Whats your name?"
-name = gets.chomp
-puts "What do you like to do?"
-fun = gets.chomp
-puts "#{name} really likes to #{fun}!!!"
+# puts "Whats your name?"
+# name = gets.chomp
+# puts "What do you like to do?"
+# fun = gets.chomp
+# puts "#{name} really likes to #{fun}!!!"
+
+#Challenge 6
+myFirstArray = []
+myFirstArray = ["one", "two", "three", "four", "five", 1, 2, 3, 4]
+puts myFirstArray.last
+puts myFirstArray.first
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray.push("true")
+puts myFirstArray[3].class
