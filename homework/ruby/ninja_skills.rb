a3808b0f8146775798979cb25384f625947ca895
MikeNewm32 <michaelnewman85@hotmail.com>
2017-08-21 22:44:42 -0400

    3. Integers Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -5,8 +5,16 @@
 # puts a != b
 
 # Challenge 2
-name = "nil"
-puts name
-puts "#{name} was here"
-print name
-puts name.class
+# name = "nil"
+# puts name
+# puts "#{name} was here"
+# print name
+# puts name.class
+
+# Challenge 3
+d = 10
+puts d
+e = 5.5
+puts e
+d = e
+puts d
