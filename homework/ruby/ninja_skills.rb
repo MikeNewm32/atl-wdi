2259d75bb9513fec502f366b0c1f613769f61a60
MikeNewm32 <michaelnewman85@hotmail.com>
2017-08-21 22:42:02 -0400

    2. Nil skills.

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,5 +1,12 @@
 # Challenge 1
-a = true
-b = false
-puts a == b
-puts a != b
+# a = true
+# b = false
+# puts a == b
+# puts a != b
+
+# Challenge 2
+name = "nil"
+puts name
+puts "#{name} was here"
+print name
+puts name.class
